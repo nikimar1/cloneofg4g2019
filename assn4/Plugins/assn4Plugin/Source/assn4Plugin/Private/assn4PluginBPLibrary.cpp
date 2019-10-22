18181db4a72959b0f82fc6b69dec61ade267ce3b
nikimar1 <32849673+nikimar1@users.noreply.github.com>
2019-10-22 18:27:21 -0400

    Created plugin

---
+++ assn4/Plugins/assn4Plugin/Source/assn4Plugin/Private/assn4PluginBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "assn4PluginBPLibrary.h"
+#include "assn4Plugin.h"
+
+Uassn4PluginBPLibrary::Uassn4PluginBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float Uassn4PluginBPLibrary::assn4PluginSampleFunction(float Param)
+{
+	return -1;
+}
+
