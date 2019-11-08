0256ecef0cde11c25e2ce887759abfdd7b6e4271
nikimar1 <32849673+nikimar1@users.noreply.github.com>
2019-11-08 17:53:40 -0500

    got a lot of file parsing done. Could have done multiple commits. have parsing of first line being image, parsing of ndims being 3 and parsing of dimsizes being 3 numbers >0, as well as parsing outputing last line of the file

--- assn5/Plugins/assn5_plugin/Source/assn5_plugin/Private/assn5LoadFactory.cpp
+++ assn5/Plugins/assn5_plugin/Source/assn5_plugin/Private/assn5LoadFactory.cpp
@@ -57,8 +57,8 @@ UObject * Uassn5LoadFactory::FactoryCreateFile(UClass * InClass, UObject * InPar
 	//get full file path
 	const FString Filepath = FPaths::ConvertRelativePathToFull(Filename);
 
-	//logging full file path
-	UE_LOG(LogTemp, Warning, TEXT("Opening file at the following path: %s"), *Filepath);
+	//logging full file path for debugging but commenting out for final project
+	//UE_LOG(LogTemp, Warning, TEXT("Opening file at the following path: %s"), *Filepath);
 	
 	//array for holding contents of file
 	TArray <FString> fileContents; 
@@ -69,12 +69,19 @@ UObject * Uassn5LoadFactory::FactoryCreateFile(UClass * InClass, UObject * InPar
 	//get size of the array of file lines so I can check if the .raw file string is last or not 
 	int32 arraySize = fileContents.Num();
 
+	//for storing some values I am parsing from file
+	int32 myNDims=-1;
+	int32 dimSizeX = -1;
+	int32 dimSizeY = -1;
+	int32 dimSizeZ = -1;
+
 	//reading over and logging array to confirm if this step worked
 	//also, taking actions based on contents of each file line
 	int count = 0;
 	for (FString myString : fileContents)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("String at line %d is %s"),count, *myString);
+		//for debugging but removed for final program
+		//UE_LOG(LogTemp, Warning, TEXT("String at line %d is %s"),count, *myString);
 		
 		//First string should show that object is image. I am assuming the string has to exactly equal this format but ignores case.
 		//Otherwise, what if there is an object type called an "Image Map" or something else with a space. 
@@ -84,75 +91,119 @@ UObject * Uassn5LoadFactory::FactoryCreateFile(UClass * InClass, UObject * InPar
 			if (!myString.Equals(TEXT("ObjectType = Image"), ESearchCase::IgnoreCase))
 			{
 				UE_LOG(LogTemp, Warning, TEXT("The first line of the file does not declare it is an image file"));
+				return nullptr;
 			}
-			else
-			{
-				UE_LOG(LogTemp, Warning, TEXT("This is an 'Image' ObjectType"));
-			}
+
+			//for debugging but removed for final program
+			//else
+			//{
+			//	UE_LOG(LogTemp, Warning, TEXT("This is an 'Image' ObjectType"));
+			//}
 		}
 
+		else if (count == (arraySize -1))
+		{
+			//for debugging purposes but commenting out for final program.
+			UE_LOG(LogTemp, Warning, TEXT("The final line is %s"), *myString);
+		}
+		else
+		{
+			//check if NDims are correct. Must equal 3 since I am undergrad
+			if (myString.Contains(TEXT("NDims")))
+			{
+				//not 3 dimensions
+				if (!myString.Equals(TEXT("NDims = 3"), ESearchCase::IgnoreCase))
+				{
+					UE_LOG(LogTemp, Warning, TEXT("Value error for NDims"));
+					return nullptr;
+				}
+				else
+				{
+					//set NDims based on file
+					myNDims = 3;
+
+					//output that ndims equals 3 for debugging. later commenting out for final project
+					//UE_LOG(LogTemp, Warning, TEXT("NDims = 3"));
+				}
+			}
+			//otherwise check if it is dim size and has an array of three different values
+			//if not 3 positive values, output error
+			else if (myString.Contains(TEXT("DimSize =")))
+			{
+				//Remove everything except dim size parameters. I am assuming it is formatted as dimsize = some dims
+				//Otherwise format is incorrect.
+				FString myDimSizes = myString.RightChop(10);
+
+				//for debugging purposes print them, but commented out in final code
+				//UE_LOG(LogTemp, Warning, TEXT("My dim sizes are %s"), *myDimSizes);
+
+				//now take these dims (should be three) and make them into an array. 
+				//if array is not of size 3 or if any number is <=0, then output an error
+
+				//making array for storing them
+				TArray <FString> myDimSizesArray;
+
+				//parse into array useing whitespace as seperator
+				myDimSizes.ParseIntoArrayWS(myDimSizesArray);
+
+				//first check if array is not right size
+				if (!(myDimSizesArray.Num() == 3))
+				{
+					UE_LOG(LogTemp, Warning, TEXT("My dim sizes have the incorrect number of dimensions"));
+					return nullptr;
+				}
+
+				//loop over the results
+				int countDim = 0;
+				for (FString dimSize : myDimSizesArray)
+				{
+					int32 tempInt = FCString::Atoi(*dimSize);
+					switch (countDim)
+					{
+						case 0:
+							if (tempInt <= 0)
+								UE_LOG(LogTemp, Warning, TEXT("Negative or 0 dimension for X"))
+							else
+								dimSizeX = tempInt;
+							break;
+						case 1:
+							if (tempInt <= 0)
+								UE_LOG(LogTemp, Warning, TEXT("Negative or 0 dimension for Y"))
+							else
+								dimSizeY = tempInt;
+							break;
+
+						case 2:
+							if (tempInt <= 0)
+								UE_LOG(LogTemp, Warning, TEXT("Negative or 0 dimension for Z/Slices"))
+							else
+								dimSizeZ = tempInt;
+							break;
+						default:
+							UE_LOG(LogTemp, Warning, TEXT("My dim sizes have the incorrect number of dimensions"));
+							return nullptr;
+							break;
+					}
+					countDim++;
+				}
+				
+				//for debugging purposes to see dimensions. comment out later for final program
+				//UE_LOG(LogTemp, Warning, TEXT("My dimensions are %d, %d, %d"), dimSizeX, dimSizeY, dimSizeZ);
+			}
+			else if ()
+			{
+
+			}
+
+		}
 		count++;
 		
 	}
 
 	//set object to my texture once I am done setting it up
 	Object = myTexture;
-
-		/*
-		GEditor->GetEditorSubsystem<UImportSubsystem>()->BroadcastAssetPreImport(this, InClass, InParent, InName, Parms);
-
-		Warn->Log(Filename);
-
-		//TStrongObjectPtr<UGLTFImportOptions> ImporterOptions(NewObject<UGLTFImportOptions>(GetTransientPackage(), TEXT("GLTF Importer Options")));
-		// show import options window
-		{
-			const FString Filepath = FPaths::ConvertRelativePathToFull(Filename);
-			const FString PackagePath = InParent->GetPathName();
-
-			//if (!GLTFImporterImpl::ShowOptionsWindow(Filepath, PackagePath, *ImporterOptions))
-			//{
-			//	bOutOperationCanceled = true;
-			//	return nullptr;
-			//}
-		}
-
-		//FGLTFImporterContext& Context = GLTFImporterModule->GetImporterContext();
-		// setup import options
-		{
-			//Context.StaticMeshFactory.SetUniformScale(ImporterOptions->ImportScale);
-			//Context.StaticMeshFactory.SetGenerateLightmapUVs(ImporterOptions->bGenerateLightmapUVs);
-		}
-
-		UObject* Object = nullptr;
-		/*if (Context.OpenFile(Filename))
-		{
-			const FString AssetName = Context.Asset.Name;
-			const FString NewPackageName = UPackageTools::SanitizePackageName(*(FPaths::GetPath(InParent->GetName()) / AssetName));
-			UObject*      ParentPackage = NewPackageName == InParent->GetName() ? InParent : CreatePackage(nullptr, *NewPackageName);
-
-			const TArray<UStaticMesh*>& CreatedMeshes = Context.CreateMeshes(ParentPackage, Flags, false);
-			Context.CreateMaterials(ParentPackage, Flags);
-			UpdateMeshes();
-
-			if (CreatedMeshes.Num() == 1)
-			{
-				Object = CreatedMeshes[0];
-			}
-			else if (CreatedMeshes.Num() != 0)
-			{
-				Object = CreatedMeshes[0]->GetOutermost();
-			}
-		}
-		*/
-		/**
-		GEditor->GetEditorSubsystem<UImportSubsystem>()->BroadcastAssetPostImport(this, Object);
-
-		//GLTFImporterImpl::ShowLogMessages(Context.GetLogMessages());
-
-		return Object;
-		*/
-
-		return Object;
+	
+	return Object;
 }
 
 #ifdef __clang__
